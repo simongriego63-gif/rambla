555d278fac8476b4fab95c9cd893efbd2cee7d5b
simongriego63-gif <simongriego63@gmail.com>
2026-05-28 19:45:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -276,3 +276,11 @@ if(btnInstalar) {
 window.addEventListener('appinstalled', () => {
     if(btnInstalar) btnInstalar.style.display = 'none';
 });
+// Activar el Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker registrado con éxito', reg.scope))
+            .catch(err => console.error('Error al registrar el Service Worker', err));
+    });
+}
