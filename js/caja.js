9a99354f51942290af6c959fd8ecceb459c93953
simongriego63-gif <simongriego63@gmail.com>
2026-05-28 19:46:19 -0300

    Add Service Worker registration to caja.js
    Register the Service Worker on window load.

--- js/caja.js
+++ js/caja.js
@@ -330,3 +330,11 @@ class BekiCajaApp {
 window.onload = () => {
     window.miCaja = new BekiCajaApp(); 
 };
+// Activar el Service Worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('Service Worker registrado con éxito', reg.scope))
+            .catch(err => console.error('Error al registrar el Service Worker', err));
+    });
+}
