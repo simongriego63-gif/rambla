19c2bc633b0002ee50ee2e9346d928e2fe1b28ee
simongriego63-gif <simongriego63@gmail.com>
2026-05-29 19:51:02 -0300

    Update Service Worker registration log messages

--- js/app-op1.js
+++ js/app-op1.js
@@ -250,12 +250,11 @@ class BekiApp {
     }
 }
 
-// Activar el Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('./sw.js')
-            .then(reg => console.log('Service Worker registrado con éxito', reg.scope))
-            .catch(err => console.error('Error al registrar el Service Worker', err));
+            .then(reg => console.log('Service Worker registrado', reg.scope))
+            .catch(err => console.error('Error Service Worker', err));
     });
 }
 
